ea48c8582dd72b1ee66926d0f51c58dd8f9018d7
azad <azadnio@gmail.com>
2022-07-06 13:19:30 -0400

    Corrections and CURLS

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express')
     , router = express.Router()
-    , Speakeasy = require("speakeasy");
+    , speakeasy = require("speakeasy");
 
 const testEmail = 'azadnio@gmail.com'
     , testPassword = 'azad12345';
 
 
 router
-    // get all the tasks
+    // get the logon credentials and create security token
     .post('/login', (req, res) => { 
 
         let { email, password } = req.body;
@@ -17,7 +17,7 @@ router
         if(email == testEmail && password === testPassword) {
 
             status = true;
-            var secret = Speakeasy.generateSecret({ length: 20 });
+            var secret = speakeasy.generateSecret({ length: 20 });
             res.send({ "secret": secret.base32, status });
         }
 
@@ -26,26 +26,34 @@ router
             res.status(401).json({status, message: 'Invalid username or password'})
     })
     
-    // create a new task
+    // generate the OTP number
     .post("/generate-otp", (request, response, next) => {
+
+
         response.send({
-            "token": Speakeasy.totp({
+            "token": speakeasy.totp({
                 secret: request.body.secret,
                 encoding: "base32"
-            }),
-            "remaining": (30 - Math.floor((new Date()).getTime() / 1000.0 % 30))
+            })
         });
     })
 
-    //update a task
+    //Validate the OTP number
     .post("/validate-otp", (request, response, next) => {
-        response.send({
-            "valid": Speakeasy.totp.verify({
-                secret: request.body.secret,
-                encoding: "base32",
-                token: request.body.token,
-                window: 0
-            })
+
+        let isValidOTP = speakeasy.totp.verify({
+            secret: request.body.secret,
+            encoding: "base32",
+            token: request.body.token,
+            window: 0
+        });
+
+        let statusCode  = isValidOTP ? 200 : 401;
+        let message     = isValidOTP ? "OTP validated!" : "Invalid or expired OTP number";
+
+        response.status(statusCode).send({
+            "valid": isValidOTP,
+            "message": message
         });
     });
 
